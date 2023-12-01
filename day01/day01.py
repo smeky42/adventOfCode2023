ee45d1a891a8191cf3f8499b108c2ffb68d063e9
Peter Neubauer <development@smeky.de>
2023-12-01 09:02:55 +0100

    day01 adding digits and reading number words

--- day01/day01.py
+++ day01/day01.py
@@ -1,5 +1,3 @@
-import re
-
 def concat_first_last_digit(string):
     return string[0] + string[-1]
     
@@ -12,12 +10,27 @@ def remove_non_digits(string):
 
     return result
 
+def replace_words_with_numbers(string):
+    string = string.replace("one","one1one")
+    string = string.replace("two","two2two")
+    string = string.replace("three","three3three")
+    string = string.replace("four","four4four")
+    string = string.replace("five","five5five")
+    string = string.replace("six","six6six")
+    string = string.replace("seven","seven7seven")
+    string = string.replace("eight","eight8eight")
+    string = string.replace("nine","nine9nine")
+
+    return string 
+    
 file = open("input.txt", "r")
 
 sum = 0
 
 for line in file:
-    only_digits = remove_non_digits(line)
+    no_number_words = replace_words_with_numbers(line)
+    print(no_number_words)
+    only_digits = remove_non_digits(no_number_words)
     first_last_concat = concat_first_last_digit(only_digits)
     sum += int(first_last_concat)
 
