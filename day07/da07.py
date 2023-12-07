b855ef996322631b4e1dab7ae97e2f746b6c5a84
Peter Neubauer <peter.neubauer@ul.com>
2023-12-07 15:07:44 +0100

    day07 joker

--- day07/da07.py
+++ day07/da07.py
@@ -4,6 +4,7 @@ def bubble_sort(arr):
     for i in range(n):
         for j in range(0, n-i-1):
             # print(f"Compare \n {arr[j]} {arr[j+1]}")
+
             if poker_hand_ranking(arr[j]) > poker_hand_ranking(arr[j+1]): #arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
             elif poker_hand_ranking(arr[j]) == poker_hand_ranking(arr[j+1]):
@@ -17,7 +18,7 @@ def bubble_sort(arr):
 def poker_card_ranking(hand1, hand2):
     hand1 = hand1.split()[0]
     hand2 = hand2.split()[0]
-    ranks = '23456789TJQKA'
+    ranks = 'J23456789TQKA'
     
     for rank1, rank2 in zip(hand1, hand2): 
         if ranks.index(rank1) > ranks.index(rank2):
@@ -32,43 +33,64 @@ def poker_card_ranking(hand1, hand2):
     
 
 def find_leading_card(hand, count):
+    if count == 1:
+        return highest_card(hand)
+
     for card in hand: 
-        if hand.count(card) == count:
-            return card 
+        if hand.count(card) == count and card != 'J':  
+            return card
+    
+    return find_leading_card(hand, count-1)
+
+    return '2'
 
 
 def highest_card(hand):
-    ranks = '23456789TJQKA'
-    highest_card = '2'
+    ranks = 'J23456789TQKA'
+    highest_card = 'J'
 
     for card in hand: 
         if ranks.index(highest_card) < ranks.index(card):
             highest_card = card
 
-    return card
+    return highest_card
 
 
 def poker_hand_ranking(hand):
     hand = hand.split()[0]
     ranks = '23456789TJQKA'
-    
+
+    # print(f"Rank hand {hand}")
+
     rank_counts = {rank: hand.count(rank) for rank in ranks}
     if len(set(hand)) == 1:
         if 5 in rank_counts.values():
             return 900 # + ranks.index(find_leading_card(hand, 5)) # Five of a kind
     elif len(set(hand)) == 2:
         if 4 in rank_counts.values():
+            if 'J' in hand: 
+                return poker_hand_ranking(hand.replace('J', find_leading_card(hand,4)))
             return 800 # + ranks.index(find_leading_card(hand, 4)) # Four of a kind
         else:
+            if 'J' in hand: 
+                return poker_hand_ranking(hand.replace('J', find_leading_card(hand,4)))
             return 700# + ranks.index(find_leading_card(hand, 3)) # Full house
     elif len(set(hand)) == 3:
         if 3 in rank_counts.values():
+            if 'J' in hand: 
+                return poker_hand_ranking(hand.replace('J', find_leading_card(hand,3)))
             return 400 #+ ranks.index(find_leading_card(hand, 3))  # Three of a kind
         else:
+            if 'J' in hand: 
+                return poker_hand_ranking(hand.replace('J', find_leading_card(hand,3)))
             return 300 #+ ranks.index(find_leading_card(hand, 2)) # Two pairs
     elif len(set(hand)) == 4:
+        if 'J' in hand: 
+            return poker_hand_ranking(hand.replace('J', find_leading_card(hand,2)))
         return 200 #+ ranks.index(find_leading_card(hand, 2))  # One pair
     elif len(set(hand)) == 5:
+        if 'J' in hand: 
+            return poker_hand_ranking(hand.replace('J', find_leading_card(hand,1)))
         return 100 #+ ranks.index(highest_card(hand)) # High card
 
 
@@ -83,17 +105,19 @@ def multiply_numbers_with_rank(hands):
 with open("input.txt", "r") as file:
     hands = file.readlines()
 
-# print(hands)
+# # print(hands)
 
 hands = bubble_sort(hands)
 # print(hands)
 with open("output.txt", 'w') as f:
-        for item in hands:
-            f.write("%s" % item)
+    for item in hands:
+        f.write("%s \n" % item.split()[0])
 
 hands = multiply_numbers_with_rank(hands)
 # print(f"Multiply {hands}")
 print(f"Sum {sum(hands)}")
 
-# 252028643 to hig
-# 252285105
+# hand = 'JJ44J'
+# print (hand)
+# print (hand.replace('J', find_leading_card(hand,2)))
+# print (poker_hand_ranking(hand))
